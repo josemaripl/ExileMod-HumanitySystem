56c05bc43170d337b1518e8aea323ef0ec350715
Gr8 <gr8_boi52@yahoo.com>
2015-12-29 13:01:54 -0800

    added missing semi colons

--- GR8Humanity/cfg/CfgExileCustomCode.hpp
+++ GR8Humanity/cfg/CfgExileCustomCode.hpp
@@ -4,11 +4,11 @@ Made By GR8
 GR8's Humanity System v1.0
 
 */
-ExileServer_object_player_createBambi 					= "GR8Humanity\createBambi.sqf"
-ExileServer_object_player_database_load 				= "GR8Humanity\database_load.sqf"
-ExileServer_object_player_event_onMpKilled 				= "GR8Humanity\onMpKilled.sqf"
-ExileServer_object_player_sendStatsUpdate 				= "GR8Humanity\sendStatsUpdate.sqf"
-ExileServer_system_network_dispatchIncomingMessage 		= "GR8Humanity\network_dispatchIncomingMessage.sqf"
+ExileServer_object_player_createBambi 					= "GR8Humanity\createBambi.sqf";
+ExileServer_object_player_database_load 				= "GR8Humanity\database_load.sqf";
+ExileServer_object_player_event_onMpKilled 				= "GR8Humanity\onMpKilled.sqf";
+ExileServer_object_player_sendStatsUpdate 				= "GR8Humanity\sendStatsUpdate.sqf";
+ExileServer_system_network_dispatchIncomingMessage 		= "GR8Humanity\network_dispatchIncomingMessage.sqf";
 ExileClient_system_network_dispatchIncomingMessage 		= "GR8Humanity\dispatchIncomingMessage.sqf";
 ExileClient_object_player_network_updateStatsRequest 	= "GR8Humanity\updateStatsRequest.sqf";
 ExileClient_object_player_network_loadPlayerResponse 	= "GR8Humanity\loadPlayerResponse.sqf";
